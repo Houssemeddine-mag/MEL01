6cd4d2f9c2ee6383b1f818c40fd78c07b6c70299
Houssemeddine-mag <houssem.magra@univ-constantine2.dz>
2025-02-17 09:49:00 +0100

    Update Mines.java

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -11,7 +11,7 @@ public class Mines extends JFrame {
 	private static final long serialVersionUID = 4772165125287256837L;
 	
 	private final int WIDTH = 250;
-    private final int  = 290;
+    private final int HEIGHT = 290;
 
     private JLabel statusbar;
     
